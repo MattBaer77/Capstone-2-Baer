c7f0b95ec0a2ae87c68bf708dc8fcf391961eab0
Matthew Baer <matt.baer77@gmail.com>
2023-12-27 01:23:19 -0500

    testing SDK with joke route on root route

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const express = require("express");
 
 const morgan = require("morgan")
 
-const SpoonacularKey = ""
+const SpoonacularKey = process.env.spoonacularKey
 
 const app = express();
 
@@ -16,31 +16,15 @@ app.use(morgan("tiny"))
 // SPOONACULAR TO TEST
 
 var SpoonacularApi = require('../spoonacularSDK/dist/com.spoonacular.client/index.js');
-
 var defaultClient = SpoonacularApi.ApiClient.instance;
+
 // Configure API key authorization: apiKeyScheme
 var apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
-apiKeyScheme.apiKey = "YOUR API KEY"
+apiKeyScheme.apiKey = SpoonacularKey
 // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
 //apiKeyScheme.apiKeyPrefix['x-api-key'] = "Token"
 
-var api = new SpoonacularApi.DefaultApi()
-var analyzeRecipeRequest = new SpoonacularApi.AnalyzeRecipeRequest(); // {AnalyzeRecipeRequest} Example request body.
-var opts = {
-  'language': 'en', // {String} The input language, either \"en\" or \"de\".
-  'includeNutrition': false, // {Boolean} Whether nutrition data should be added to correctly parsed ingredients.
-  'includeTaste': false // {Boolean} Whether taste data should be added to correctly parsed ingredients.
-};
-
-var callback = function(error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('API called successfully. Returned data: ' + data);
-  }
-};
-
-// api.analyzeRecipe(analyzeRecipeRequest, opts, callback);
+let apiInstance = new SpoonacularApi.MiscApi();
 
 // SPOONACULAR TO TEST
 
@@ -49,8 +33,15 @@ app.get('/', (req, res) => {
     console.log("ROOT");
     // console.log(req);
 
-    return res.json({message:"Root"})
-
+    apiInstance.getARandomFoodJoke((error, data, response) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('API called successfully. Returned data: ' + data);
+        console.log(response.body.text)
+        return res.json(response.body)
+      }
+    });
 
 });
 
