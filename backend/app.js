da90cabaaa2fb05404c0aa1b78812a38b6b4ab40
Matthew Baer <matt.baer77@gmail.com>
2023-12-29 19:07:31 -0500

    added error handling - 404 and General - to app

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,38 @@
 "use strict";
 
 const express = require("express");
-
 const morgan = require("morgan")
+const ExpressError = require("./expressError");
 
 const app = express();
 app.use(express.json())
 app.use(morgan("tiny"))
 
-const jokesRoutes = require("./routes/jokes")
+const jokesRoutes = require("./routes/jokes");
 
 app.use("/jokes", jokesRoutes)
+
+// 404 Error Handling
+app.use((req, res, next) => {
+
+    const notFoundError = new ExpressError("Not Found!!!", 404)
+
+    return next(notFoundError);
+
+})
+
+// Generic Error Handler
+app.use((err, req, res, next) => {
+
+    // if(process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: {message, status},
+    });
+
+})
+
 
 module.exports = app;
--- backend/routes/jokes.js
+++ backend/routes/jokes.js
@@ -9,19 +9,19 @@ const { MiscApi } = require("../config")
 const router = express.Router({ mergeParams: true });
 
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
 
-    console.log("Joke Test Route");
+  console.log("Joke Test Route");
 
-    MiscApi.getARandomFoodJoke((error, data, response) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log('API called successfully. Returned data: ' + data);
-        console.log(response.body.text)
-        return res.json(response.body)
-      }
-    });
+  MiscApi.getARandomFoodJoke((error, data, response) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('API called successfully. Returned data: ' + data);
+      console.log(response.body.text)
+      return res.json(response.body)
+    }
+  });
 
 });
 
