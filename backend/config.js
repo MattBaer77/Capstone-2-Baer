89766a2d74768a25258854bc24fa92bf4e4674f6
Matthew Baer <matt.baer77@gmail.com>
2023-12-31 00:18:27 -0500

    added DatabaseUri and db setup

--- backend/config.js
+++ backend/config.js
@@ -6,6 +6,13 @@ require("colors");
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+    return (process.env.NODE_ENV === "test")
+        ? "easyspoon_test"
+        : process.env.DATABASE_URL || "easyspoon";
+}
+
 const spoonacularKey = process.env.spoonacularKey
 
 console.log("Config:".green)
@@ -19,6 +26,7 @@ module.exports = {
 
     SECRET_KEY,
     PORT,
+    getDatabaseUri,
     spoonacularKey
     
 }
