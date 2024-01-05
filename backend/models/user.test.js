ef18949cfd179b6802e677947d79e71726417003
Matthew Baer <matt.baer77@gmail.com>
2024-01-04 21:22:41 -0500

    users.test "register" - ALL

--- backend/models/user.test.js
+++ backend/models/user.test.js
@@ -134,3 +134,57 @@ describe("authenticate", function () {
 
 });
 
+//  register
+
+describe("register", function () {
+
+    const newUser = {
+      username: "new",
+      firstName: "Test",
+      lastName: "Tester",
+      email: "test@test.com",
+      isAdmin: false,
+    };
+  
+    test("works", async function () {
+      let user = await User.register({
+        ...newUser,
+        password: "password",
+      });
+      expect(user).toEqual(newUser);
+      const found = await db.query("SELECT * FROM users WHERE username = 'new'");
+      expect(found.rows.length).toEqual(1);
+      expect(found.rows[0].is_admin).toEqual(false);
+      expect(found.rows[0].password.startsWith("$2b$")).toEqual(true);
+    });
+  
+    test("works: adds admin", async function () {
+      let user = await User.register({
+        ...newUser,
+        password: "password",
+        isAdmin: true,
+      });
+      expect(user).toEqual({ ...newUser, isAdmin: true });
+      const found = await db.query("SELECT * FROM users WHERE username = 'new'");
+      expect(found.rows.length).toEqual(1);
+      expect(found.rows[0].is_admin).toEqual(true);
+      expect(found.rows[0].password.startsWith("$2b$")).toEqual(true);
+    });
+  
+    test("bad request with dup data", async function () {
+      try {
+        await User.register({
+          ...newUser,
+          password: "password",
+        });
+        await User.register({
+          ...newUser,
+          password: "password",
+        });
+        fail();
+      } catch (err) {
+        expect(err instanceof ExpressError).toBeTruthy();
+      }
+    });
+
+});
