4c89ce32c52eede42b8cdc6e569e0a74a7814846
Matthew Baer <matt.baer77@gmail.com>
2024-01-17 22:23:58 -0500

    spoonModel.test mocks issue

--- backend/models/spoonModel.test.js
+++ backend/models/spoonModel.test.js
@@ -2,6 +2,28 @@
 
 const ExpressError = require("../expressError");
 
+const mockRandomResponse = {
+            
+    recipes: [
+    
+        {
+            id : 1,
+            title: "Faux Recipe 1"
+        },
+        
+        {
+            id : 2,
+            title: "Faux Recipe 2"
+        },
+
+        {
+            id : 3,
+            title: "Faux Recipe 3"
+        }
+
+    ]
+};
+
 jest.mock('./spoonModel', () => {
     // Import the actual module and store it in a variable
     const originalSpoonModel = jest.requireActual('./spoonModel');
@@ -10,7 +32,10 @@ jest.mock('./spoonModel', () => {
         ...originalSpoonModel,
         SpoonApi: {
             ...originalSpoonModel.SpoonApi,
-            getRandomRecipes: jest.fn(),
+            // getRandomRecipes: jest.fn(),
+            // fetchFreshData: jest.fn()
+            getRandomRecipes:jest.fn().mockImplementation(() => mockRandomResponse),
+            fetchFreshData:jest.fn().mockImplementation(() => mockRandomResponse),
         },
     };
 });
@@ -19,21 +44,41 @@ jest.mock('./spoonModel', () => {
 // Now you can safely import SpoonApi from the mock
 const { SpoonApi } = require('./spoonModel');
 
+console.log(SpoonApi.getRandomRecipes)
+
 describe("getRandomRecipes Mock Test", () => {
 
     test("mock test works", async () => {
 
-        SpoonApi.getRandomRecipes.mockReturnValue("Yee")
+        // SpoonApi.getRandomRecipes.mockReturnValue(mockRandomResponse)
+
+        console.log(SpoonApi)
 
         const opts = {
             limitLicense: true,
-            number: 1
+            number: 3
         };
 
         const fauxResponse = await SpoonApi.getRandomRecipes(opts)
-
-        expect(fauxResponse).toEqual("Yee")
+        console.log(fauxResponse)
+        expect(fauxResponse).toEqual(mockRandomResponse)
 
     });
+
+});
+
+describe("test serveRecipesCache - success", () => {
+
+    test("serveRecipesCache - No Cache Currently Saved", async () => {
+
+        console.log(SpoonApi)
+
+        const fauxResponse = await SpoonApi.serveRecipesCache()
+
+        console.log(fauxResponse)
+
+        expect(fauxResponse).toEqual(mockRandomResponse)
+
+    })
 
 });
