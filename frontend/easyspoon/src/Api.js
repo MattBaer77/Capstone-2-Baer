ed595c01b02c9650c83922d064cacfc339bb6938
Matthew Baer <matt.baer77@gmail.com>
2024-02-09 01:00:45 -0500

    completed API methods in EasySpoonAPI in Api.js

--- frontend/easyspoon/src/Api.js
+++ frontend/easyspoon/src/Api.js
@@ -10,10 +10,13 @@ class EasySpoonAPI {
 
     static token;
 
-    static async request(endpoint, data = {}, method = "GET") {
-        console.debug("API Call:", endpoint, data, method);
+    static async request(endpoint, data = {}, params = {}, method = "GET") {
+        console.debug("API Call:", endpoint, data, params, method);
 
-        const url = `${VITE_BASE_URL}/${endpoint}`;
+        const queryString = new URLSearchParams(params).toString();
+        console.log(queryString)
+
+        const url = `${VITE_BASE_URL}/${endpoint}${queryString ? `?${queryString}` : ''}`;
         const headers = { Authorization: `Bearer ${this.token}` };
         const body = (method === "GET") ? undefined : JSON.stringify(data);
 
@@ -29,11 +32,11 @@ class EasySpoonAPI {
         }
     }
 
-    // USER
+    // /auth
 
     static async loginUser(userInfo) {
 
-        let res = await this.request(`auth/token`, userInfo, "POST")
+        let res = await this.request(`auth/token`, userInfo, {}, "POST");
 
         return res.token
 
@@ -41,23 +44,17 @@ class EasySpoonAPI {
 
     static async signUpUser(userInfo) {
 
-        let res = await this.request(`auth/register`, userInfo, "POST")
+        let res = await this.request(`auth/register`, userInfo, {}, "POST");
 
         return res.token
 
     };
 
-    static async editUser(userInfo, username) {
-
-        let res = await this.request(`users/${username}`, userInfo, "PATCH")
-
-        return res
-
-    };
+    // /users
 
     static async getUserInfo(username) {
 
-        let res = await this.request(`users/${username}`)
+        let res = await this.request(`users/${username}`);
 
         return res
 
@@ -65,25 +62,153 @@ class EasySpoonAPI {
 
     static async getUserDetails(username) {
 
-        let res = await this.request(`users/${username}/details`)
+        let res = await this.request(`users/${username}/details`);
 
         return res
 
     };
 
-    // RECIPES
+    static async editUser(userInfo, username) {
+
+        let res = await this.request(`users/${username}`, userInfo, {}, "PATCH");
+
+        return res
+
+    };
+
+    static async deleteUser(username) {
+
+        let res = await this.request(`users/${username}`, {}, {}, "DELETE");
+
+        return res
+
+    };
+
+    static async getUserCache(username) {
+
+        let res = await this.request(`users/${username}/cache`);
+
+        return res
+
+    };
+
+    static async getUserCacheOnly(username) {
+
+        let res = await this.request(`users/${username}/cache-only`);
+
+        return res
+
+    };
+
+    // /recipes
 
     static async getRecipesCache() {
 
-        let res = await this.request(`recipes/cache`)
+        let res = await this.request(`recipes/cache`);
 
         return res
         
-    }
+    };
 
-    // INGREDIENTS
+    static async getRecipesSearch(params) {
 
-    // GROCERYLISTS
+        let res = await this.request(`recipes/cache`, {}, params, "GET");
+
+        return res
+        
+    };
+
+    static async getRecipeById(id) {
+
+        let res = await this.request(`recipes/${id}`);
+
+        return res
+
+    };
+
+    static async getRecipeByIdWithNutrition(id) {
+
+        let res = await this.request(`recipes/${id}/nutrition`);
+
+        return res
+
+    };
+
+    // /ingredients
+
+    static async getIngredientsSearch(params) {
+
+        let res = await this.request(`ingredients/search`, {}, params, "GET");
+
+        return res
+
+    };
+
+    static async getIngredientById(id) {
+
+        let res = await this.request(`ingredients/${id}`);
+
+        return res
+
+    };
+
+    // /grocery-lists
+
+    static async getAllUsersGroceryLists(username) {
+
+        let res = await this.request(`/grocery-lists/${username}/all`);
+
+        return res
+
+    };
+
+    static async getGroceryListById(id) {
+
+        let res = await this.request(`/grocery-lists/${id}`);
+
+        return res
+
+    };
+
+    static async postIngredientToGroceryList(id, ingredientData) {
+
+        let res = await this.request(`/grocery-lists/${id}/ingredients`, ingredientData, {}, "POST");
+
+        return res
+
+    };
+
+    static async editIngredientOnGroceryList(id, ingredientId, ingredientData) {
+
+        let res = await this.request(`/grocery-lists/${id}/ingredients/${ingredientId}`, ingredientData, {}, "PATCH");
+
+        return res
+
+    };
+
+    static async deleteIngredientOnGroceryList(id, ingredientId) {
+
+        let res = await this.request(`/grocery-lists/${id}/ingredients/${ingredientId}`, {}, {}, "DELETE");
+
+        return res
+
+    };
+
+    static async postRecipeToGroceryList(id, recipeId) {
+
+        let res = await this.request(`/grocery-lists/${id}/recipes/${recipeId}`, {}, {}, "POST");
+
+        return res
+
+    };
+
+    static async deleteRecipeOnGroceryList(id, recipeId) {
+
+        let res = await this.request(`/grocery-lists/${id}/recipes/${recipeId}`, {}, {}, "DELETE");
+
+        return res
+
+    };
 
 }
 
