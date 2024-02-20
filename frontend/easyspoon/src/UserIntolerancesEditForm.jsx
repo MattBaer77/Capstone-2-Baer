b407fa6ed1983b755f5f2c8e6f724458a8032bad
Matthew Baer <matt.baer77@gmail.com>
2024-02-19 22:58:54 -0500

    UserIntolerancesEditForm debug / comments removed

--- frontend/easyspoon/src/UserIntolerancesEditForm.jsx
+++ frontend/easyspoon/src/UserIntolerancesEditForm.jsx
@@ -53,55 +53,8 @@ const UserIntolerancesEditForm = () => {
 
     }, [currentUser]);
 
-    // const handleToggle = async(e) => {
-
-    //     // e.preventDefault();
-
-    //     try {
-
-    //         console.log(e)
-
-    //         const {id, name, checked} = e.target
-    //         // Just logging check to start
-    //         if(checked) {
-
-    //             await currentUser.userApi.addUserIntolerance(currentUser.username, e.target.id)
-
-    //             setFormData((data) => [...data, {intoleranceId:id, intoleranceName:name}])
-    //             console.log(formData)
-
-    //         } else if(!checked) {
-
-    //             await currentUser.userApi.deleteUserIntolerance(currentUser.username, e.target.id)
-
-    //             const updatedFormData = formData.filter(i => i.intoleranceId !== id);
-    //             setFormData(() => [...updatedFormData])
-    //             console.log(formData)
-
-    //         }
-    //         // await loadUser(currentUser.token)
-
-    //         // setFormData(() => {
-
-    //         //     console.log(currentUser.intolerances)
-    //         //     return [...currentUser.intolerances]
-    
-    //         // })
-            
-    //         // console.log(e.target.checked)
-    //         // console.log(e.target.name)
-    //         // console.log(e.target.id)
-
-    //     } catch (e) {
-    //         console.log(e)
-    //         setError(e)
-    //     }
-
-    // }
-
     const handleToggle = async (e) => {
 
-        // e.preventDefault();
         try {
 
             const { id, checked } = e.target;
@@ -110,13 +63,7 @@ const UserIntolerancesEditForm = () => {
             } else {
                 await currentUser.userApi.deleteUserIntolerance(currentUser.username, id);
             }
-            // Reload user data after toggle operation
             await loadUser(currentUser.token);
-            // Update formData with the latest user intolerances
-
-
-            // setFormData(() => [...currentUser.intolerances]);
-
 
         } catch (error) {
             console.log(error);
@@ -145,8 +92,8 @@ const UserIntolerancesEditForm = () => {
 
         <div className="Form UserIntolerancesForm">
 
-            <p>{JSON.stringify(intolerancesAll)}</p>
-            <p>{JSON.stringify(formData)}</p>
+            {/* <p>{JSON.stringify(intolerancesAll)}</p> */}
+            {/* <p>{JSON.stringify(formData)}</p> */}
 
             <form>
 
@@ -162,14 +109,7 @@ const UserIntolerancesEditForm = () => {
                             id={intolerance.id}
                             key={intolerance.id}
                             name={intolerance.intoleranceName}
-                            checked={formData.some(i => {
-
-                                // console.log((i.intoleranceId))
-                                // console.log(intolerance)
-
-                                return i.intoleranceId === intolerance.id
-
-                            })}
+                            checked={formData.some(i => i.intoleranceId === intolerance.id)}
                         />
 
                         <label htmlFor={intolerance.intoleranceName}>{intolerance.intoleranceName}</label>
