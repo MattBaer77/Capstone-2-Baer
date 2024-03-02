e3f4369dde51723e69385e5a8520f05b739953f7
Matthew Baer <matt.baer77@gmail.com>
2024-03-01 19:08:00 -0500

    moved frontend/easyspoon to easyspoon

--- frontend/easyspoon/src/tests/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react'
-import App from '../App'
-
-it('renders correctly', () => {
-
-  const app = render(<App />);
-  expect(app).toBeDefined()
-  
-})
