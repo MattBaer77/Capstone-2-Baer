37968aca01e93e16ee9575d90bbd496841b0ef41
Matthew Baer <matt.baer77@gmail.com>
2024-07-30 09:14:49 -0400

    backend/config.js - added commented code - modified getDatabaseURI

--- backend/config.js
+++ backend/config.js
@@ -6,12 +6,20 @@ require("colors");
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
+// ORIGINAL VERSION - pattern matched from previous projects. SEE COMMENTS BELOW FOR CONTEXT
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
     return (process.env.NODE_ENV === "test")
         ? "easyspoon_test"
         : process.env.DATABASE_URL || "easyspoon";
 }
+
+// Modified in post branch. SEE MIKAEL EMAIL FOR CONTEXT.
+// function getDatabaseUri() {
+//     return (process.env.NODE_ENV === "test")
+//         ? "postgresql:///easyspoon_test"
+//         : process.env.DATABASE_URL || "postgresql:///easyspoon";
+// }
 
 const spoonacularKey = process.env.spoonacularKey
 
